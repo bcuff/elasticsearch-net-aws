f0360a9536c3f240308b6e4f3b7af66307130844
Brandon Cuff <brandoncuff@gmail.com>
2016-10-18 11:05:12 -0700

    Strong signing

--- src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/Properties/AssemblyInfo.cs
+++ src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/Properties/AssemblyInfo.cs
@@ -34,4 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.3.0")]
 [assembly: AssemblyFileVersion("1.2.3.0")]
+#if DEBUG
 [assembly: InternalsVisibleTo("Tests")]
+#endif
