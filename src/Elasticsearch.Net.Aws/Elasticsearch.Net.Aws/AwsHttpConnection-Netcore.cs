0a09d5d9bd0860e8d224b53be75d7051089397f6
Brandon Cuff <brandoncuff@gmail.com>
2016-11-10 12:40:42 -0800

    Added netcore AwsHttpConnection

---
+++ src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/AwsHttpConnection-Netcore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Elasticsearch.Net.Aws
+{
+    public class AwsHttpConnection : HttpConnection
+    {
+        readonly Credentials _credentials;
+        readonly string _region;
+
+        public AwsHttpConnection(AwsSettings settings)
+        {
+            // todo - proper credential chain
+            _credentials = new Credentials
+            {
+                AccessKey = settings.AccessKey,
+                SecretKey = settings.SecretKey,
+            };
+            _region = settings.Region;
+        }
+
+        protected override HttpRequestMessage CreateHttpRequestMessage(RequestData requestData)
+        {
+
+            var result = base.CreateHttpRequestMessage(requestData);
+            var signable = new SignableHttpRequestMessage(result, requestData);
+            SignV4Util.SignRequest(signable, _credentials, _region, "es");
+            return result;
+        }
+    }
+}
---
+++ src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/SignableHttpRequestMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+
+namespace Elasticsearch.Net.Aws
+{
+    internal class SignableHttpRequestMessage : IHttpRequest
+    {
+        readonly HttpRequestMessage _request;
+        readonly RequestData _requestData;
+
+        public SignableHttpRequestMessage(HttpRequestMessage request, RequestData requestData)
+        {
+            _request = request;
+            _requestData = requestData;
+        }
+
+        public Uri Uri => _request.RequestUri;
+        public string Method => _request.Method.Method;
+        public byte[] Body
+        {
+            get
+            {
+                // note - this probably won't work if compression is enabled
+                if (_requestData?.PostData == null) return null;
+                if (_requestData.PostData.WrittenBytes != null) return _requestData.PostData.WrittenBytes;
+                using (var ms = new MemoryStream())
+                {
+                    _requestData.PostData.Write(ms, _requestData.ConnectionSettings);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public IEnumerable<string> GetHeaderKeys() => _request.Headers.Select(h => h.Key);
+
+        public IEnumerable<string> GetHeaderValues(string name) => _request.Headers.GetValues(name);
+
+        public void SetHeader(string name, string value)
+        {
+            _request.Headers.Remove(name);
+            _request.Headers.Add(name, value);
+        }
+    }
+}
