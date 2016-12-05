97dc8b32a71805fc3ae4205a2c44c83aaf2ed3a6
Brandon Cuff <brandoncuff@gmail.com>
2016-12-05 12:16:52 -0800

    Fixed assembly signing

--- src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/Properties/AssemblyInfo.cs
+++ src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/Properties/AssemblyInfo.cs
@@ -18,4 +18,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0de39cec-a00f-409a-9d45-aafa31c77ed9")]
 
+#if !DEBUG
+[assembly: AssemblyKeyFile("key.snk")]
+#else
 [assembly: InternalsVisibleTo("ElasticSearch.Net.Aws.Tests")]
+#endif
