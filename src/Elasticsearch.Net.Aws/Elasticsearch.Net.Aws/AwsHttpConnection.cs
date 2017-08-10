32eea5be6405dad8d748a5b780e3430af3ef8d49
Troy Larson <troylar@gmail.com>
2017-08-10 17:08:33 -0400

    Removed unnecessary using

--- src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/AwsHttpConnection.cs
+++ src/Elasticsearch.Net.Aws/Elasticsearch.Net.Aws/AwsHttpConnection.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using System.Net;
 using System.Net.Http;
 
 namespace Elasticsearch.Net.Aws
@@ -102,3 +101,4 @@ namespace Elasticsearch.Net.Aws
         }
     }
 }
+
