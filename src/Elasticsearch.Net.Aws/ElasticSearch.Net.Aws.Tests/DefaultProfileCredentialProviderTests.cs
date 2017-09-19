2a7ce5a3065466e6ee5bb12483a205aed01e68e3
Toby Heighway <theighway@seven.com.au>
2017-09-19 11:54:29 +1000

    Add DefaultProfileCredentialProvider
    Requires dependency on core AWS SDK

---
+++ src/Elasticsearch.Net.Aws/ElasticSearch.Net.Aws.Tests/DefaultProfileCredentialProviderTests.cs
@@ -0,0 +1,22 @@
+﻿using Elasticsearch.Net.Aws;
+using NUnit.Framework;
+
+namespace ElasticSearch.Net.Aws.Tests
+{
+    [TestFixture]
+    public class DefaultProfileCredentialProviderTests
+    {
+        [TestCase]
+        [Ignore("Requires setting up an aws profile with the expected values")]
+        public void GetCredentials_should_return_expected_values()
+        {
+            var credentialProvider = new DefaultProfileCredentialProvider();
+            var credentials = credentialProvider.GetCredentials();
+
+            Assert.NotNull(credentials);
+            Assert.AreEqual("TestKey", credentials.AccessKey);
+            Assert.AreEqual("TestSecret", credentials.SecretKey);
+            Assert.AreEqual("TestToken", credentials.Token);
+        }
+    }
+}
