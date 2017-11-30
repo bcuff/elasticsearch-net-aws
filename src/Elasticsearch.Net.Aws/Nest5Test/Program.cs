6c80275814e3bedd84d2218e14a521d3309a723b
Brandon Cuff <brandoncuff@gmail.com>
2017-11-29 18:08:30 -0800

    Added Nest5Test to make sure nest5 works

---
+++ src/Elasticsearch.Net.Aws/Nest5Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Elasticsearch.Net.Aws;
+using Nest;
+using Elasticsearch.Net;
+
+namespace Nest5Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpConnection = new AwsHttpConnection("us-east-1");
+            var pool = new SingleNodeConnectionPool(new Uri("https://someendpoint.us-east-1.es.amazonaws.com"));
+            var config = new ConnectionSettings(pool, httpConnection);
+            var client = new ElasticClient(config);
+            client.Ping();
+            Console.WriteLine("pinged successfully");
+        }
+    }
+}
