99b06c79103abc2ca840cd2fe83a8da79283c41e
Markus Thurner <thoean@gmail.com>
2018-01-19 08:21:00 +0100

    Upgrade to v6 of ElasticSearch, NEST and ElasticSearch.Net

--- src/Elasticsearch.Net.Aws/ElasticSearch.Net.Aws.IntegrationTests/PingTests.cs
+++ src/Elasticsearch.Net.Aws/ElasticSearch.Net.Aws.IntegrationTests/PingTests.cs
@@ -1,19 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Elasticsearch.Net;
 using Elasticsearch.Net.Aws;
 using NUnit.Framework;
 
 namespace IntegrationTests
 {
-    using System.IO;
-
     [TestFixture]
     public class PingTests
     {
+        public class TestDoc
+        {
+            public string Id { get; set; }
+            public string Something { get; set; }
+        }
+        private class Response : IElasticsearchResponse
+        {
+            public bool TryGetServerErrorReason(out string reason)
+            {
+                reason = "error";
+                return true;
+            }
+
+            public IApiCallDetails ApiCall { get; set; }
+        }
+
         private static string Region => TestConfig.AwsSettings.Region;
         private static ICredentialsProvider Credentials => new StaticCredentialsProvider(TestConfig.AwsSettings);
+
+
+        [Test]
+        public async Task Indexing_documents_should_work()
+        {
+            var httpConnection = new AwsHttpConnection(Region, Credentials);
+            var pool = new SingleNodeConnectionPool(new Uri(TestConfig.Endpoint));
+            var config = new Nest.ConnectionSettings(pool, httpConnection);
+            var client = new Nest.ElasticClient(config);
+            var indexName = "integration-test";
+            var existsResponse = await client.IndexExistsAsync(indexName);
+            if (existsResponse.Exists)
+            {
+                await client.DeleteIndexAsync(indexName);
+            }
+            var createIndexResponse = await client.CreateIndexAsync(indexName);
+            Assert.IsTrue(createIndexResponse.Acknowledged);
+
+            var doc = new TestDoc { Id = "123", Something = "unit-test" };
+            var indexResponse = await client.IndexAsync(doc, i => i.Id(doc.Id).Index(indexName));
+            Assert.AreEqual(Nest.Result.Created, indexResponse.Result);
+            var queryResponse = await client.SearchAsync<TestDoc>(s => s.Index(indexName).Query(q => q.Ids(id => id.Values(doc.Id))));
+            Assert.AreEqual(queryResponse.Documents.Count, 1);
+            Assert.AreEqual(queryResponse.Documents.First().Id, doc.Id);
+            Assert.AreEqual(queryResponse.Documents.First().Something, doc.Something);
+            var deleteIndexResonse = await client.DeleteIndexAsync(indexName);
+            Assert.IsTrue(deleteIndexResonse.Acknowledged);
+        }
         [Test]
         public void Ping_should_work()
         {
@@ -21,8 +63,8 @@ namespace IntegrationTests
             var pool = new SingleNodeConnectionPool(new Uri(TestConfig.Endpoint));
             var config = new ConnectionConfiguration(pool, httpConnection);
             var client = new ElasticLowLevelClient(config);
-            var response = client.Ping<object>();
-            Assert.AreEqual(200, response.HttpStatusCode.GetValueOrDefault(-1));
+            var response = client.Ping<Response>();
+            Assert.AreEqual(200, response.ApiCall.HttpStatusCode.GetValueOrDefault(-1));
 
         }
 
@@ -36,7 +78,7 @@ namespace IntegrationTests
             var response = client.Ping();
             Assert.AreEqual(true, response.IsValid);
         }
-
+        
         [Test]
         public void Random_encoded_url_should_work()
         {
@@ -45,8 +87,8 @@ namespace IntegrationTests
             var pool = new SingleNodeConnectionPool(new Uri(TestConfig.Endpoint));
             var config = new ConnectionConfiguration(pool, httpConnection);
             var client = new ElasticLowLevelClient(config);
-            var response = client.Get<Stream>(randomString, string.Join(",", Enumerable.Repeat(randomString, 2)), randomString);
-            Assert.AreEqual(404, response.HttpStatusCode.GetValueOrDefault(-1));
+            var response = client.Get<Response>(randomString, string.Join(",", Enumerable.Repeat(randomString, 2)), randomString);
+            Assert.AreEqual(404, response.ApiCall.HttpStatusCode.GetValueOrDefault(-1));
         }
 
         [Test]
@@ -56,8 +98,8 @@ namespace IntegrationTests
             var pool = new SingleNodeConnectionPool(new Uri(TestConfig.Endpoint));
             var config = new ConnectionConfiguration(pool, httpConnection);
             var client = new ElasticLowLevelClient(config);
-            var response = client.Get<Stream>("index*", "type", "id");
-            Assert.AreEqual(404, response.HttpStatusCode.GetValueOrDefault(-1));
+            var response = client.Get<Response>("index*", "type", "id");
+            Assert.AreEqual(404, response.ApiCall.HttpStatusCode.GetValueOrDefault(-1));
         }
     }
 
